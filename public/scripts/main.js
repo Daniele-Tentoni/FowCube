3af41b227d92e19dfd2d14903a2f67fe52c95820
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-01-20 15:09:52 +0100

    Improvements on web app.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,4 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import {MDCRipple} from '@material/ripple';
 'use strict';
 
 // Signs-in Friendly Chat.
@@ -341,7 +327,7 @@ var imageFormElement = document.getElementById('image-form');
 var mediaCaptureElement = document.getElementById('mediaCapture');
 var userPicElement = document.getElementById('user-pic');
 var userNameElement = document.getElementById('user-name');
-var signInButtonElement = document.getElementById('sign-in');
+const signInButtonElement = new MDCRipple(document.getElementById('sign-in'));
 var signOutButtonElement = document.getElementById('sign-out');
 var signInSnackbarElement = document.getElementById('must-signin-snackbar');
 
